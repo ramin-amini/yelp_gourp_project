02594f857f8e717c99969a90ca2652ef3f9b8ea6
Prajna S <prajnas@growperson-lm.corp.yahoo.com>
2013-10-31 20:01:42 -0700

    Added call support when a user clicks on the phone number .

--- src/com/codepath/apps/yelpclient/ImageViewActivity.java
+++ src/com/codepath/apps/yelpclient/ImageViewActivity.java
@@ -4,16 +4,20 @@ package com.codepath.apps.yelpclient;
 import com.codepath.apps.yelpclient.models.Business;
 import com.loopj.android.image.SmartImageView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class ImageViewActivity extends Activity {
 	TextView name; 
 	TextView caption; 
 	TextView address;
-	//TextView phone;
+	TextView phone;
 
 
 	@Override
@@ -27,17 +31,17 @@ public class ImageViewActivity extends Activity {
 		ivImage.setImageUrl(smImage.substring(0,smImage.lastIndexOf("/")+1) + "l.jpg");
 		name = (TextView) findViewById(R.id.nameText);
 		address = (TextView) findViewById(R.id.addressView);
-		//phone = (TextView) findViewById(R.id.phone);
+		phone = (TextView) findViewById(R.id.phoneNumber);
 		caption = (TextView) findViewById(R.id.captionView);
 		String resultCaption = result.getCaption();
-		if(resultCaption.equalsIgnoreCase("null" ))
+		if(resultCaption.equalsIgnoreCase("null"))
 			caption.setText("");	//we can put some text here		
 		else 
 			caption.setText(resultCaption);
 		Business info = (Business) getIntent().getSerializableExtra("businessInfo");
 		name.setText(info.getName() + "\nRating: " + info.getRating());
-		address.setText(info.getAddress() + info.getPhone());
-		//phone.setText(info.getString("phone"));
+		address.setText(info.getAddress());
+	    phone.setText(info.getPhone());
 	}
 
 	@Override
@@ -45,5 +49,13 @@ public class ImageViewActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.image_view, menu);
 		return true;
+	}
+	
+	public void callAction(View v) {
+	    
+	    Intent callIntent = new Intent(Intent.ACTION_CALL);
+	    callIntent.setData(Uri.parse("tel:"+phone.getText().toString().trim()));
+	    callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+	    startActivity(callIntent);
 	}
 }
